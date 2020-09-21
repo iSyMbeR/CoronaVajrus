b48ae9103ee65c50824ea36000634fcb79851c6b
Kamil Lobasiuk <kamil.lobasiuk@gmail.com>
2020-09-21 10:05:12 +0200

    start of project

---
+++ src/main/java/kom/symber/koronavajrus/KoronaVajrusApplication.java
@@ -0,0 +1,14 @@
+package kom.symber.koronavajrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoronaVajrusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoronaVajrusApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/kom/symber/koronavajrus/KoronaVajrusApplicationTests.java
@@ -0,0 +1,13 @@
+package kom.symber.koronavajrus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoronaVajrusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
